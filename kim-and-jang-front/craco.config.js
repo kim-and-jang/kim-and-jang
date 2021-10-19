d3ce836922b60f9be3a260ecacb0b0b0051a51f3
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-10-19 16:48:48 +0900

    craco alias 수정

--- kim-and-jang-front/craco.config.js
+++ kim-and-jang-front/craco.config.js
@@ -1,13 +1,16 @@
 const CracoAlias = require("craco-alias");
+const path = require("path");
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: "tsconfig",
-        tsConfigPath: "tsconfig.paths.json",
-      },
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@typings": path.resolve(__dirname, "src/typings"),
     },
-  ],
+  },
 };
