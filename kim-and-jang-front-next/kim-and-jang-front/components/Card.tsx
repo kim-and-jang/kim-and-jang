80b59023ac0cf6e30e33b28ff20c6fe871411ce1
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-10-25 17:56:30 +0900

    card list 화면 추가

--- kim-and-jang-front-next/kim-and-jang-front/components/Card.tsx
+++ kim-and-jang-front-next/kim-and-jang-front/components/Card.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { FC } from "react";
+import { ICard, ICardList } from "@typings/db";
+
+interface Props {
+  card: ICard;
+}
 
 const cardStyle = {
   border: "1px solid #DDD",
 };
 
-const Card = () => {
-  return <div style={cardStyle}>card</div>;
+const Card: FC<Props> = ({ card }) => {
+  return <div style={cardStyle}>card {card.title}</div>;
 };
 
 export default Card;
--- kim-and-jang-front-next/kim-and-jang-front/components/CardList.tsx
+++ kim-and-jang-front-next/kim-and-jang-front/components/CardList.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react";
 import { ICard, ICardList } from "@typings/db";
+import Card from "./Card";
 
 interface Props {
   cards: ICard[];
 }
 
 const CardList: FC<Props> = ({ cards }) => {
-  return <div>CardList</div>;
+  return (
+    <>
+      <div>CardList</div>
+      {cards.map((card) => (
+        <Card key={card.id} card={card} />
+      ))}
+    </>
+  );
 };
 
 export default CardList;
