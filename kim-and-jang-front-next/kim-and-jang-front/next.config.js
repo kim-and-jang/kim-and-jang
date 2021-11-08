56986398a28f15b38086e55c32973ceef94544d8
mong <momomingzhi@gmail.com>
2021-11-09 01:10:33 +0900

    feat: calender 추가

--- kim-and-jang-front-next/kim-and-jang-front/next.config.js
+++ kim-and-jang-front-next/kim-and-jang-front/next.config.js
@@ -1,15 +1,16 @@
 /** @format */
 
-const { config } = require('@fullcalendar/react');
-
+const { config } = require("@fullcalendar/react");
+// const withSass = require("@zeit/next-sass");
+// module.exports = withSass();
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    webpack: (config) => {
-        // config.module.rules.push({
-        //     test: /\.css$/i,
-        //     use: ['style-loader', 'css-loader'],
-        // });
-        return config;
-    },
+  reactStrictMode: true,
+  webpack: (config) => {
+    // config.module.rules.push({
+    //     test: /\.css$/i,
+    //     use: ['style-loader', 'css-loader'],
+    // });
+    return config;
+  },
 };
--- kim-and-jang-front-next/kim-and-jang-front/pages/calender/index.tsx
+++ kim-and-jang-front-next/kim-and-jang-front/pages/calender/index.tsx
@@ -1,19 +1,64 @@
 /** @format */
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '@components/Layout';
-import FullCalendar from '@fullcalendar/react'; // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-import './main.scss';
-const calender = () => {
-    return (
-        <Layout>
-            <FullCalendar
-                plugins={[dayGridPlugin]}
-                initialView='dayGridMonth'
-            />
-        </Layout>
-    );
+import { CalendarMode } from "antd/lib/calendar/generateCalendar";
+
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Layout from "@components/Layout";
+// import FullCalendar from "@fullcalendar/react"; // must go before plugins
+// import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+// import "../../styles/calendar.scss";
+import {
+  MonthlyBody,
+  MonthlyCalendar,
+  MonthlyDay,
+  MonthlyNav,
+  DefaultMonthlyEventItem,
+} from "@zach.codes/react-calendar";
+import { format, subHours, startOfMonth } from "date-fns";
+import "@zach.codes/react-calendar/dist/calendar-tailwind.css";
+interface calenderEvent {
+  title: string;
+  date: Date;
+}
+export const Calender = () => {
+  let [currentMonth, setCurrentMonth] = useState<Date>(
+    startOfMonth(new Date())
+  );
+  let eventArray: calenderEvent[] = [
+    { title: "Call John", date: subHours(new Date(), 2) },
+    { title: "Call John", date: subHours(new Date(), 1) },
+    { title: "Meeting with Bob", date: new Date() },
+  ];
+  return (
+    <>
+      <MonthlyCalendar
+        currentMonth={currentMonth}
+        onCurrentMonthChange={(date) => setCurrentMonth(date)}
+      >
+        <MonthlyNav />
+        <MonthlyBody
+          events={[
+            { title: "Call John", date: subHours(new Date(), 2) },
+            { title: "Call John", date: subHours(new Date(), 1) },
+            { title: "Meeting with Bob", date: new Date() },
+          ]}
+        >
+          <MonthlyDay
+            renderDay={(data: any) =>
+              data.map((item: any, index: any) => (
+                <DefaultMonthlyEventItem
+                  key={index}
+                  title={item.title}
+                  // Format the date here to be in the format you prefer
+                  date={format(item.date, "k:mm")}
+                />
+              ))
+            }
+          />
+        </MonthlyBody>
+      </MonthlyCalendar>
+    </>
+  );
 };
-export default calender;
+export default Calender;
